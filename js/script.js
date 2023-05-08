0e3b99323321b2aa5aac569f2a24141609d77978
RodrigoBrasileiro <rm98952@fiap.com.br>
2023-05-08 09:53:38 -0300

    commit may8

--- js/script.js
+++ js/script.js
@@ -28,28 +28,82 @@
 //Random
 //Math.Ceil
 //Math.Random
-//Math.florr
+//Math.florr // que joga os números para baixo
+//math.round aredonda os números aleatoriamente
 
- function mudarCor() {
-    let r = 0;
-    let g = 0;
-    let b = 0;
+//  function mudarCor() {
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
 
-    r = Math.round (Math.random() * 255);
-    g = Math.round(Math.random() * 255);
-    b = Math.round(Math.random() * 255);
-    const cabecalho = document.querySelector(".cabecalho");
-    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+//     r = Math.round (Math.random() * 255);
+//     g = Math.round(Math.random() * 255);
+//     b = Math.round(Math.random() * 255);
+//     const cabecalho = document.querySelector(".cabecalho");
+//     cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    tmp = setTimeout( mudarCor, 1000);
+//     tmp = setTimeout( mudarCor, 1000);
 
-    const banner1 = document.querySelector(".banner-1");
-    banner1.style.backgroundColor = `rgb(${r},${g},${b})`;
+//     const banner1 = document.querySelector(".banner-1");
+//     banner1.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    const banner2 = document.querySelector(".banner-2");
-    banner2.style.backgroundColor = `rgb(${r},${g},${b})`;
- }
- mudarCor();
- function mudarbanner() {
-    const banner1 = document.querySelector(".banner-1");
- }
+//     const banner2 = document.querySelector(".banner-2");
+//     banner2.style.backgroundColor = `rgb(${r},${g},${b})`;
+//  }
+//  mudarCor();
+//  function mudarbanner() {
+//     const banner1 = document.querySelector(".banner-1");
+//  }
+
+
+
+// function mudabanner(){
+//    const imgElement1 = document.querySelector(".banner-1 img");
+//    // console.log(imgElement1);
+//    let nr = Math.ceil(Math.random()*3);
+//    imgElement1.src = `./img/banner-lateral-${nr}.gif`;
+//    setTimeout(mudabanner2 , 2000 );
+// }
+
+
+// function mudabanner2(){
+//    const imgElement2 = document.querySelector(".banner-2 img");
+//    // console.log(imgElement1);
+//    let nr = Math.ceil(Math.random()*3);
+//    imgElement2.src = `./img/banner-lateral-${nr}.gif`;
+//    setTimeout(mudabanner , 2000 );
+// }
+
+// mudabanner();
+
+function mudabanner(){
+   const imgElement = document.querySelector(".banner-1 img");
+   // console.log(imgElement1);
+   let nr = Math.ceil(Math.random()*3);
+   imgElement.src = `./img/banner-lateral-${nr}.png`;
+
+   const imgElement2 = document.querySelector(".banner-2 img");
+   // console.log(imgElement1);
+   nr = Math.ceil(Math.random()*3);
+   imgElement2.src = `./img/banner-lateral-${nr}.png`;
+
+   setTimeout(mudabanner , 2000 );
+}
+
+mudabanner();
+
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function (){
+   if(this.textContent == "LIGAR"){
+      const imgElement = document.querySelector("img[alt=´Lampada apagada´]");
+      imgElement.src = "./img/pic_bulbon.gif";
+      imgElement.alt = "Lampada acesa";
+      this.textContent = "DESLIGAR";
+   }else{
+      const imgElement = document.querySelector("img[alt=´Lampada acesa´]");
+      imgElement.src = "./img/pic_buloff.gif";
+      imgElement.alt = "Lampada apagada";
+      this.textContent = "LIGAR";
+   }
+});
