005c09136c155c2de95c41bd3b1b45a869bd4019
RodrigoBrasileiro <rm98952@fiap.com.br>
2023-05-22 09:51:12 -0300

    commit ultima aula

--- js/login.js
+++ js/login.js
@@ -1,20 +1,104 @@
+//https://dontpad.com/1ESPF-2023
 "use strict";
-let usuario = "";
-let senha = "";
-const botaoSubmit = document.querySelector("#btnSubmit");
-botaoSubmit.addEventListener("click",()=>{
+
+//LISTA DE USUÁRIOS
+let listaDeUsuario = [
+
+    {
+        nomeCompleto : "José das Couves",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456" 
+    },
+    {
+        nomeCompleto : "Ruivão Lindão",
+        nomeUsuario : "ruivo",
+        senhaUsuario : "123456" 
+    },
+    {
+        nomeCompleto : "Filipino de Deus",
+        nomeUsuario : "fili",
+        senhaUsuario : "123456" 
+    },
+    {
+        nomeCompleto : "Pedro José",
+        nomeUsuario : "pepe",
+        senhaUsuario : "123456" 
+    },
+    {
+        nomeCompleto : "Ricardito Nelson",
+        nomeUsuario : "rico",
+        senhaUsuario : "123456" 
+    }
+];
+
+
+
+
+//Listener Global
+addEventListener("click", (evt)=>{
+    
     const inputUser = document.querySelector("input[name='nmUser']");
     const inputPass = document.querySelector("input[name='pass']");
+    
     const labelUser = document.querySelector("label[for='idUser']");
     const labelPass = document.querySelector("label[for='idPass']");
+    
+    const msgStaus = document.querySelector("#msg");
 
-    if(inputUser.value.length > 3 && inputPass.value.length > 3) {
-        // inputUser.setAttribute("style","outline-color:#00ff00;")
-        // inputPass.setAttribute("style","outline-color:#00ff00;")
-        labelUser.setAttribute("style","color:#00ff00;");
-        labelPass.setAttribute("style","color:#00ff00;");
-    }else{
-        labelUser.setAttribute("style","color:#ff0000;");
-        labelPass.setAttribute("style","color:#ff0000;");
+    //Criando o OBJETO USUARIO-VALIDADO
+    let usuarioValidado = {};
+
+    //Recuperar o conteúdo dos campos e adicionar em um OBJETO
+    //CRIANDO O OBJETO USUARIO-LOGADO
+    const usuarioLogado = {
+        nomeUsuarioLogado : inputUser.value,
+        senhaUsuarioLogado: inputPass.value
     }
+
+    if(evt.target.id == "btnSubmit"){
+        
+        if(inputUser.value.length > 3 && inputPass.value.length > 3){
+            labelUser.setAttribute("style","color:#00ff00;");
+            labelPass.setAttribute("style","color:#00ff00;");
+
+            //Se preencheu a qtd de caractéres necessária agora vamos validar o nomeUsuário e a senhaUsuário
+
+            //Chamando a lista de usuários e iterando para criar a validação para cada usuário contido nela.
+            listaDeUsuario.forEach( (usuario)=>{
+
+                if(usuarioLogado.nomeUsuarioLogado == usuario.nomeUsuario && usuarioLogado.senhaUsuarioLogado == usuario.senhaUsuario){
+                    usuarioValidado = usuario;
+                }
+                
+            });
+
+
+            if(usuarioValidado.nomeUsuario != undefined){
+                msgStaus.setAttribute("style","color:#00ff00;");
+                msgStaus.innerHTML = `<span><strong>O usuário ${usuarioValidado.nomeCompleto} realizou o login com Sucesso!!</strong></span>`;
+
+                //Redirect
+
+                setTimeout(()=>{
+                    window.location.href = "../sucesso.html";
+                },3000);
+
+                
+
+            }else{
+                msgStaus.setAttribute("style","color:#ff0000;");
+                msgStaus.innerHTML = "<span><strong>Login ou senha incorretos...</strong></span>";
+            }
+
+
+        }else{
+            labelUser.setAttribute("style","color:#ff0000;");
+            labelPass.setAttribute("style","color:#ff0000;");
+        }
+
+
+
+   }
+
+
 });
